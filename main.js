69416e1d0b4624b70a81d7e536ea6db03673ba3d
NikaPankvelashvili <npanqvela@gmail.com>
2024-07-26 04:21:36 +0400

    First Commit

---
+++ main.js
@@ -0,0 +1,47 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+import { RGBELoader } from "three/examples/jsm/Addons.js";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+const rgbeLoader = new RGBELoader();
+rgbeLoader.load(
+  "https://threejs.org/examples/textures/memorial.hdr", // HDR texture URL
+  function (texture) {
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    scene.background = texture;
+
+    const geometry = new THREE.BoxGeometry(10, 10, 10);
+    const material = new THREE.MeshBasicMaterial({
+      map: texture,
+      side: THREE.BackSide,
+    });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+  }
+);
+
+camera.position.set(2.5, 2.5, 2.5);
+
+orbit.enablePan = false;
+orbit.enableZoom = false;
+
+function animate() {
+  requestAnimationFrame(animate);
+  orbit.update();
+  renderer.render(scene, camera);
+}
+
+animate();
